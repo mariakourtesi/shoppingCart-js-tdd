f10c1bda450fc6eff98ba867d0af012f7b843d8b
Maria <mkourtesi@hotmail.com>
2021-07-10 11:01:34 +0100

    add offer tests

--- _tests_/checkOffer.spec.js
+++ _tests_/checkOffer.spec.js
@@ -9,5 +9,27 @@ describe('the offer calculator', function() {
 		expect(getOfferTotal).toBeTruthy();
 	});
 
+    describe('when there are no offer', function() {
+		it('should calculate offer price as 0 and remaining untotalled items as quantity passed in', function() {
+			//A is 50 each of 3 for 130
+			let result = getOfferTotal('A', 4);
+			expect(result).toEqual({
+				offerTotal:0,
+				unmatchedItems:4
+			});
+		});
+	});
+
+    describe('when there is a matching offer', function() {
+		it('should calculate offer price and remaining untotalled items', function() {
+			//A is 50 each the offer is 3 for 130
+			addOffer('A',3,130);
+			var result = getOfferTotal('A', 4);
+			expect(result).toEqual({
+				offerTotal:130,
+				unmatchedItems:1
+			});
+		});
+	});
 	
 });
